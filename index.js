fb3a7f80178c818e842c6aff6476b60a195d0054
Alessio Dionisi <me@alessiodionisi.com>
2016-11-22 19:10:51 +0100

    fast startup

--- index.js
+++ index.js
@@ -39,21 +39,14 @@ LGTv2.prototype.getState = function(callback) {
 
 LGTv2.prototype.checkInterval = function(callback) {
   var self = this
-  var lgtv = require('lgtv2')({
-    url: 'ws://' + this.ip + ':3000',
-    timeout: 4000,
-    reconnect: 0
-  })
-  lgtv.on('connect', function() {
-    self.log('LGTv2 connect')
-    self.powered = true
-    lgtv.disconnect()
-    return callback(null, true)
-  })
-  lgtv.on('error', function() {
-    self.log('LGTv2 error')
-    self.powered = false
-    setTimeout(self.checkInterval.bind(self, callback), 5000)
+  ping.sys.probe(this.ip, function(isAlive) {
+    if (!isAlive) {
+      self.powered = false
+      setTimeout(self.checkInterval.bind(self, callback), 5000)
+    } else {
+      self.powered = true
+      return callback(null, true)
+    }
   })
 }
 
